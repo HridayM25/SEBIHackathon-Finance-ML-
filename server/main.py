0885703169c86e0830fb1053e273558072571364
Mardav Chirag Gandhi <gandhi.mardav@gmail.com>
2023-08-19 23:44:02 +0530

    [Server]: Added Google Search Functionality

--- server/main.py
+++ server/main.py
@@ -3,6 +3,7 @@ from typing import Annotated
 from fastapi import FastAPI, File, Form, UploadFile
 from fastapi.responses import FileResponse
 from fastapi.middleware.cors import CORSMiddleware
+from utils.google_search import google_search
 
 
 app = FastAPI()
@@ -15,8 +16,12 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
+def search(query):
+    return google_search(query)
+
 @app.get("/")
-def home():
+def get_all_ipos():
+    search("Current Live IPO's in India")
     return {"IPOWiseAI": "Works"}
 
 
