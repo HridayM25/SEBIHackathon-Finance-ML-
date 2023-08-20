6bd1beccec201ccf7f2c0b9dea0ecc9e0e213d6c
Mardav Chirag Gandhi <gandhi.mardav@gmail.com>
2023-08-20 12:20:51 +0530

    [Server] : Autoloading IPO Docs

--- server/utils/analyze_selected_ipo.py
+++ server/utils/analyze_selected_ipo.py
@@ -1,3 +1,6 @@
+from utils.google_search import google_search, search_ipo
+
 def analyze_selected_ipo(ipo_name, ipo_link):
     print("Analyzing IPO: ", ipo_name)
     print("Link: ", ipo_link) 
+    search_ipo(ipo_name)
--- server/utils/google_search.py
+++ server/utils/google_search.py
@@ -1,8 +1,13 @@
 from googlesearch import search
-from utils.site_scraper import get_site_data
+from utils.site_scraper import get_site_data, get_pdf
 
 def google_search(query):
     for j in search(query, tld="co.in", num=10, stop=10, pause=2):
         # get_site_data(j)
         print(j)
 
+def search_ipo(ipo_name):
+    query = ipo_name + " ipo pdf sebi"
+    for j in search(query, tld="co.in", num=10, stop=1, pause=2):
+        # print(j)
+        get_pdf(j)
